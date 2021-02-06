3935af34a493623a3e9422082ecfbed8cfeb36ab
nithish-mucherla <mucherlanithu@gmail.com>
2021-02-07 01:06:26 +0530

    Updates

---
+++ precache-manifest.56756af129841579904435ca62aaac37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0227373c2b51f74ec665276b3cddbd1",
+    "url": "/opiniate/index.html"
+  },
+  {
+    "revision": "245e10e1ecf1e19072f0",
+    "url": "/opiniate/static/css/main.8b67abcc.chunk.css"
+  },
+  {
+    "revision": "8fb01df3dcf07b0472fe",
+    "url": "/opiniate/static/js/2.f299a9e6.chunk.js"
+  },
+  {
+    "revision": "245e10e1ecf1e19072f0",
+    "url": "/opiniate/static/js/main.4be15923.chunk.js"
+  },
+  {
+    "revision": "1f00a1c04686ed899353",
+    "url": "/opiniate/static/js/runtime-main.bdf08cde.js"
+  },
+  {
+    "revision": "edf18a80796b039acf88b9df24dff17e",
+    "url": "/opiniate/static/media/loader.edf18a80.gif"
+  }
+]);
